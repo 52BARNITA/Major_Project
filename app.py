a33e0cb2ebb6b28e851fdfc53538422464711180
Barnita Paul <barnitapaul214@gmail.com>
2022-08-04 04:07:03 +0530

    commited

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def hello_World():
-    return "Hello"
+    return "Python"
 if __name__=="__main__":
     app.run(debug=True)
